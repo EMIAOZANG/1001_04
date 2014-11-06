96d0fc4e294792c3ed4f51a0ca5f35e44c38fa2f
luchristopher <luchristopher@192.168.101.164>
2014-11-06 00:51:17 -0500

    2nd

--- Desktop/pydev_projects/1001_04/main.py
+++ Desktop/pydev_projects/1001_04/main.py
@@ -5,6 +5,8 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm, linear_model
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import *
+from sklearn.metrics import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,6 +20,7 @@ def cleanBosonData(file_name):
     #count values in 'Label' and create the 'Y' column as the boolean indicator
     counter = raw_data['Label'].groupby(raw_data['Label']).count().keys()[0]
     raw_data['Y'] = (raw_data['Label']!=counter).astype(int)  
+    raw_data = raw_data.drop('Label',1)
     #create dummy variables
     missing_exist = (raw_data==-999).any(axis=0)
     column_list = missing_exist[(missing_exist == True)].keys()
@@ -50,11 +53,54 @@ def fit_n_plot(arg_train,arg_test,lab):
     plotAUC(arg_test[lab], mm.decision_function(arg_test.drop(lab,1)), 'SVM')
     plt.show()
 
+#compute the aucs for each test folds
+def xValSVM(dataset,label_name,k,cs):
+    cv = KFold(n=dataset.shape[0], n_folds = k)
+    aucs = {}
+    for train_index,test_index in cv:
+        train_f = dataset.iloc[train_index]
+        validation_f = dataset.iloc[test_index] 
+        for c in cs:
+            svmachine = svm.SVC(kernel='linear',C=c)
+            svmachine.fit(train_f.drop(label_name,1),2*train_f[label_name]-1)
+            print validation_f[label_name]
+            metric = roc_auc_score(validation_f[label_name], svmachine.decision_function(validation_f.drop(label_name,1)))
+            
+            if (aucs.has_key(c)):
+                aucs[c].append(metric)
+            else:
+                aucs[c] = [metric]
+    return aucs
+
+def norm(dataset):
+    normalized = (dataset - dataset.mean())/(dataset.max()-dataset.min())
+    return normalized
+         
+        
+def execXValidation(dataset,label_name):
+    extracted_dataset = dataset.drop(['DER_deltaeta_jet_jetmv','DER_mass_jet_jetmv','DER_prodeta_jet_jetmv','DER_mass_MMCmv'],1)
+#     print extracted_dataset
+#     print extracted_dataset.shape[1]
+    normalized_dataset = norm(extracted_dataset.ix[:,range(extracted_dataset.shape[1]-1)])
+    normalized_dataset[label_name]=extracted_dataset[label_name]
+#     print normalized_dataset
+    cs = [10**i for i in range(-9,2)]
+    aucs = xValSVM(normalized_dataset, label_name, 10, cs)  
+    aucs_df = pd.DataFrame(aucs)
+#     print aucs_df
+    aucs_mean = aucs_df.mean()
+    aucs_stderr = aucs_df.std()
+    aucs_diff = aucs_mean-aucs_stderr
+    print aucs_diff
+                  
+        
 
 def main():
-    train = cleanBosonData('boson_training_cut_2000.csv').drop('EventID',axis=1)
-    test = cleanBosonData('boson_training_cut.csv').drop('EventID',axis=1)
-    fit_n_plot(train,test,'Y')
+    train = cleanBosonData('boson_training_cut_2000.csv').drop('EventId',1)
+    print train
+    test = cleanBosonData('boson_testing_cut.csv').drop('EventId',1)
+    #fit_n_plot(train,test,'Y')
+    execXValidation(train, 'Y')
     
 if __name__ == "__main__":
     main()
